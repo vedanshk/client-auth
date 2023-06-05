71afad06a6ea3cde1b4fd86cfbbaed6e846e279f
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-06-05 10:42:57 +0530

    Updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,42 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-
-function Header() {
-
-
-
-
-    return (
+import { connect } from "react-redux";
+function Header(props) {
+  function renderLinks() {
+    if (props.authenticated) {
+      return (
         <div>
-            <ul>
-                <li>
-                    <Link to="/">Redux Auth</Link>
-                </li>
-                <li>
-                    <Link to="/signUp">SignUp</Link>
-                </li>
-                <li>
-                    <Link to="/signin">Sign In</Link></li>
-            </ul>
-
-
-            <Link to="/signout">Sign Out</Link>
-            <Link to="/feature">Feature</Link>
-
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
         </div>
+      );
+    } else {
+      return (
+        <div>
+          <ul>
 
-    );
+            <li>
+              <Link to="/signUp">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <Link to="/">Redux Auth</Link>
+      {renderLinks()}
+    </div>
+  );
 }
 
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
 
-
-
-
-export default Header;
+export default connect(mapStateToProps)(Header);
